735517c86d8fd2b81bfa2adccfbc3cd99870e1cc
med-hcore <mehdi.ca2018@gmail.com>
2020-12-01 11:22:02 +0100

    introduction au DOM

---
+++ script.js
@@ -0,0 +1,18 @@
+let paragraphe = document.getElementById('animation');
+paragraphe.style.color = "red";
+paragraphe.style.fontSize = '30px';
+
+let spans = document.querySelector('.spantest');
+spans.style.backgroundColor = "yellow";
+
+
+
+
+
+
+
+
+
+
+
+
